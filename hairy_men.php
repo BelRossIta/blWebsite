6a1ede78757806ac0e52e6a45ce573245cb089b2
BDRossIsBoring <143637506+BDRossIsBoring@users.noreply.github.com>
2024-12-20 22:13:41 -0600

    New Project
    A BL website

---
+++ hairy_men.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!-- head, meta stuff is in includes file -->
+<head>
+    <?php
+        include('includes/header.inc.php');
+    ?>
+    <title>Booties 'R' Us</title>
+</head>
+<!-- body -->
+<body>
+    <header>
+        <?php include('includes/nav.inc.php');?>
+    </header>
+    <!-- main -->
+    <main>
+
+    </main>
+    
+</body>
+<!-- footer -->
+<footer>
+    <?php include('includes/footer.inc.php') ;?>
+</footer>
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!-- head, meta stuff is in includes file -->
+<head>
+    <?php
+        include('includes/header.inc.php');
+    ?>
+    <title>Booties 'R' Us</title>
+</head>
+<!-- body -->
+<body>
+    <header>
+        <h1>Booties 'R' Us</h1>
+        <?php include('includes/nav.inc.php');?>
+    </header>
+    <!-- main -->
+    <main>
+        <h2>Home to BL Content, Merch and More</h2>
+        <img class="splash" src="images/erHaBed.png" alt="landing page image">
+        
+    </main>
+    
+</body>
+<!-- footer -->
+<footer>
+    <?php include('includes/footer.inc.php') ;?>
+</footer>
+</html>
