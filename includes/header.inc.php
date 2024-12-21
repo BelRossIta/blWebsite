5f2a24a40688710abd4d8f126176dff674895e5a
BDRossIsBoring <143637506+BDRossIsBoring@users.noreply.github.com>
2024-12-20 22:35:51 -0600

    Add files via upload

---
+++ includes/header.inc.php
@@ -0,0 +1,11 @@
+
+    <meta charset="UTF-8">
+    <meta name='description' content="Find your fav booty here">
+    <meta name='keyword' content="Porn, Gay Porn, Anime Porn">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Stylesheet -->
+    <link rel="stylesheet"  href="css/styles.css">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&display=swap" rel="stylesheet">
